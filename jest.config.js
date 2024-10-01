c4d6e3f77178c816416dcedc10a60b485cbdd32b
Peter Zhu <zhujiaxi@amazon.com>
2024-10-01 16:54:47 -0400

    Add check-version-bump workflow and junit.xml report to codecov (#14)
    * Add check-version-bump workflow and junit.xml report to codecov
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Tweak the action
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Tweak the action
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Bump version 0.1.1
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    ---------
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  reporters: ['default', 'jest-junit'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
