8d30286413ffdfe5c06b8aa7a4478d5c27cc8336
Peter Zhu <zhujiaxi@amazon.com>
2024-10-24 17:53:22 -0400

    Improve octokit test
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- test/utility/probot/octokit.test.ts
+++ test/utility/probot/octokit.test.ts
@@ -8,11 +8,12 @@
  */
 
 import { octokitAuth } from '../../../src/utility/probot/octokit';
-import { Probot, Logger } from 'probot';
+import { Probot, ProbotOctokit, Logger } from 'probot';
 
 describe('octokitFunctions', () => {
   let app: Probot;
   let installationId: number;
+  let octokitMock: ProbotOctokit
 
   beforeEach(() => {
     app = new Probot({ appId: 1, secret: 'test', privateKey: 'test' });
@@ -20,11 +21,27 @@ describe('octokitFunctions', () => {
       info: jest.fn(),
       error: jest.fn(),
     } as unknown as Logger;
+
+    octokitMock = {} as ProbotOctokit;
+    app.auth = jest.fn().mockResolvedValue(octokitMock);
+
+    installationId = 123;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   describe('octokitAuth', () => {
     it('should fail if no installation id', async () => {
-      await expect(octokitAuth(app, installationId)).rejects.toThrowError('Please provide installation id of your github app!');
+      await expect(octokitAuth(app, undefined as any)).rejects.toThrowError('Please provide installation id of your github app!');
+    });
+
+    it('should call app.auth if installationId present and return ProbotOctokit', async () => {
+      const result = await octokitAuth(app, installationId);
+
+      expect(app.auth).toHaveBeenCalledWith(installationId);
+      expect(result).toBe(octokitMock);
     });
   });
 });
