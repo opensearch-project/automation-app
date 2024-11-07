49a179b98802badc5808adca7cda0093dc859fa5
Brandon Shien <bshien@amazon.com>
2024-11-06 17:10:51 -0800

    Remove resource config validation for github-events-to-s3 call
    Signed-off-by: Brandon Shien <bshien@amazon.com>

--- src/call/github-events-to-s3.ts
+++ src/call/github-events-to-s3.ts
@@ -12,30 +12,31 @@
 
 import { Probot } from 'probot';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { Resource } from '../service/resource/resource';
-import { validateResourceConfig } from '../utility/verification/verify-resource';
 
-export default async function githubEventsToS3(app: Probot, context: any, resource: Resource): Promise<void> {
-  if (!(await validateResourceConfig(app, context, resource))) return;
+export default async function githubEventsToS3(app: Probot, context: any): Promise<void> {
+  // Removed validateResourceConfig to let this function listen on all repos, and filter for only the repos that are public.
+  // This is done so when a new repo is made public, this app can automatically start processing its events.
+  // if (!(await validateResourceConfig(app, context, resource))) return;
+  if (context.payload.repository?.private === false) {
+    const repoName = context.payload.repository?.name;
+    const eventName = context.payload.action === undefined ? context.name : `${context.name}.${context.payload.action}`;
 
-  const repoName = context.payload.repository?.name;
-  const eventName = context.payload.action === undefined ? context.name : `${context.name}.${context.payload.action}`;
+    context.uploaded_at = new Date().toISOString();
 
-  context.uploaded_at = new Date().toISOString();
+    const now = new Date();
+    const [day, month, year] = [now.getDate(), now.getMonth() + 1, now.getFullYear()].map((num) => String(num).padStart(2, '0'));
 
-  const now = new Date();
-  const [day, month, year] = [now.getDate(), now.getMonth() + 1, now.getFullYear()].map((num) => String(num).padStart(2, '0'));
-
-  try {
-    const s3Client = new S3Client({ region: String(process.env.REGION) });
-    const putObjectCommand = new PutObjectCommand({
-      Bucket: String(process.env.OPENSEARCH_EVENTS_BUCKET),
-      Body: JSON.stringify(context),
-      Key: `${eventName}/${year}-${month}-${day}/${repoName}-${context.id}`,
-    });
-    await s3Client.send(putObjectCommand);
-    app.log.info('GitHub Event uploaded to S3 successfully.');
-  } catch (error) {
-    app.log.error(`Error uploading GitHub Event to S3 : ${error}`);
+    try {
+      const s3Client = new S3Client({ region: String(process.env.REGION) });
+      const putObjectCommand = new PutObjectCommand({
+        Bucket: String(process.env.OPENSEARCH_EVENTS_BUCKET),
+        Body: JSON.stringify(context),
+        Key: `${eventName}/${year}-${month}-${day}/${repoName}-${context.id}`,
+      });
+      await s3Client.send(putObjectCommand);
+      app.log.info('GitHub Event uploaded to S3 successfully.');
+    } catch (error) {
+      app.log.error(`Error uploading GitHub Event to S3 : ${error}`);
+    }
   }
 }
